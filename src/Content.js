0fb1879230bfd2a5b947661dc03375f2b44704bc
NapassakornKarawapong <s5935512086@phuket.psu.ac.th>
2019-09-09 07:55:32 +0700

    first commit

---
+++ src/Content.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+export default class Content extends Component{
+    constructor(){
+        super()
+        this.state={
+            count:0,
+            name:''
+        }
+    }
+
+    addNum=()=>{
+        this.setState({
+            count: this.state.count+1,
+            name: 'Click'
+        })
+    }
+    render(){
+        return(
+            <div>
+                name: {this.state.name}
+                <button onClick={this.addNum}>
+                    count:{this.state.count}
+                </button>
+            </div>
+            
+        );
+    }
+}
