aa55d1a94cba410e0262898690f2c4a58af941a8
NapassakornKarawapong <s5935512086@phuket.psu.ac.th>
2019-09-09 08:15:56 +0700

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import Content from './Content';
 import './App.css';
-
-let x = 70;
 class App extends Component {
  render() {
  return (
  <div className="App">
  Hello World
- <Content />
  </div>
  );
  }
